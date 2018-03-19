e9fe1c7293e07d9ffcd78be339f0369167899a28
Thomas Gredin <t.gredin@gmx.fr>
2018-03-19 22:13:25 +0100

    Ajout de fonction pour la matrice de model dans Entity.

--- CharacterAnimation/Entity.cpp
+++ CharacterAnimation/Entity.cpp
@@ -4,10 +4,81 @@ namespace mjt
 {
 	Entity::Entity()
 	{
-		m_modelMatrix = glm::mat4(1.f);
 	}
 
 	Entity::~Entity()
 	{
+	}
+
+	glm::mat4 Entity::getModelMatrix()
+	{
+		glm::mat4 model = glm::mat4(1.f);
+		model = glm::translate(model, m_position) *
+			glm::rotate(model, m_rotX, glm::vec3(1, 0, 0)) *
+			glm::rotate(model, m_rotX, glm::vec3(0, 1, 0)) *
+			glm::rotate(model, m_rotX, glm::vec3(0, 0, 1)) *
+			glm::scale(model, m_scale);
+
+		return model;
+	}
+
+	glm::vec3 Entity::getPosition()
+	{
+		return m_position;
+	}
+
+	glm::vec3 Entity::getScale()
+	{
+		return m_scale;
+	}
+
+	float Entity::getRotationX()
+	{
+		return m_rotX;
+	}
+
+	float Entity::getRotationY()
+	{
+		return m_rotY;
+	}
+
+	float Entity::getRotationZ()
+	{
+		return m_rotZ;
+	}
+
+	void Entity::setPosition(glm::vec3 position)
+	{
+		m_position = position;
+	}
+
+	void Entity::setPosition(float x, float y, float z)
+	{
+		setPosition(glm::vec3(x, y, z));
+	}
+
+	void Entity::setScale(glm::vec3 scale)
+	{
+		m_scale = scale;
+	}
+
+	void Entity::setScale(float sx, float sy, float sz)
+	{
+		setScale(glm::vec3(sx, sy, sz));
+	}
+
+	void Entity::setRotationX(float rotX)
+	{
+		m_rotX = rotX;
+	}
+
+	void Entity::setRotationY(float rotY)
+	{
+		m_rotY = rotY;
+	}
+
+	void Entity::setRotationZ(float rotZ)
+	{
+		m_rotZ = rotZ;
 	}
 }
--- CharacterAnimation/Entity.h
+++ CharacterAnimation/Entity.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace mjt
 {
@@ -12,7 +12,11 @@ namespace mjt
 	class Entity
 	{
 	private:
-		glm::mat4 m_modelMatrix;
+		glm::vec3 m_position;
+		glm::vec3 m_scale;
+		float m_rotX;
+		float m_rotY;
+		float m_rotZ;
 
 	public:
 		Entity();
@@ -20,5 +24,20 @@ namespace mjt
 
 		virtual void update() = 0;
 		virtual void render() = 0;
+
+		glm::mat4 getModelMatrix();
+		glm::vec3 getPosition();
+		glm::vec3 getScale();
+		float getRotationX();
+		float getRotationY();
+		float getRotationZ();
+
+		void setPosition(glm::vec3 position);
+		void setPosition(float x, float y, float z);
+		void setScale(glm::vec3 scale);
+		void setScale(float sx, float sy, float sz);
+		void setRotationX(float rotX);
+		void setRotationY(float rotY);
+		void setRotationZ(float rotZ);
 	};
 }
