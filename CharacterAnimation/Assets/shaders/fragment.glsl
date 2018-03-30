73e7bb0f07d5b7a7bf336c1ad960cc0bdc13cad0
Thomas Gredin <t.gredin@gmx.fr>
2018-03-30 23:24:02 +0200

    Documentation des sources des shaders.

--- CharacterAnimation/Assets/shaders/fragment.glsl
+++ CharacterAnimation/Assets/shaders/fragment.glsl
@@ -1,28 +1,35 @@
 #version 330 core
 
+// Subroutine declaration
 subroutine vec4 outFragColor();
 
+// Texture0
 uniform sampler2D texture;
+// Subroutine uniform
 subroutine uniform outFragColor fragColor;
 
 in vec2 outUvs;
 in vec3 outColor;
 
+// Subroutine to use with TexturedModel
 subroutine(outFragColor)
 vec4 outFromTexture()
 {
 	return texture2D(texture, outUvs);
 }
 
+// Subroutine to use with ColoredModel
 subroutine(outFragColor)
 vec4 outFromColor()
 {
 	return vec4(outColor.xyz, 1.0);
 }
 
+// Out color for processed fragment
 out vec4 frag_color;
 
 void main()
 {
+	// Attrib fragment color
     frag_color = fragColor();
 }
--- CharacterAnimation/Assets/shaders/vertex.glsl
+++ CharacterAnimation/Assets/shaders/vertex.glsl
@@ -1,25 +1,32 @@
 #version 330 core
 
+// Access vertex position data
 layout(location=0) in vec3 position;
+// Access uvs for the vertex
 layout(location=1) in vec2 uvs;
+// Access color for the vertex
 layout(location=2) in vec3 color;
 
-/* Define subroutine type */
+// Define subroutine type
 subroutine void passOutData();
 
-/* Subroutine to call */
+// Subroutine to call
 subroutine uniform passOutData outData;
 uniform mat4 mvp;
 
+// Pass uvs to fragment shader
 out vec2 outUvs;
+// Pass color to fragment shader
 out vec3 outColor;
 
+// Subroutine to use with TexturedModel
 subroutine(passOutData)
 void passOutUvs()
 {
 	outUvs = uvs;
 }
 
+// Subroutine to use with ColoredModel
 subroutine(passOutData)
 void passOutColor()
 {
@@ -28,6 +35,8 @@ void passOutColor()
 
 void main()
 {
+	// Set the position of the processed vertex
 	gl_Position = mvp * vec4(position, 1.0);
+	// Fill output data to fragment shader with appropriate subroutine
 	outData();
 }
