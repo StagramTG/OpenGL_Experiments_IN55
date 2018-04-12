635e571f2ffb3dced4a4a6402a97f624ccd5dcda
Thomas Gredin <t.gredin@gmx.fr>
2018-04-12 16:08:15 +0200

    Ajout d'une fonction de création de modèle de carré.

--- CharacterAnimation/MJT/Utils/MeshUtils.cpp
+++ CharacterAnimation/MJT/Utils/MeshUtils.cpp
@@ -1,6 +1,35 @@
 #include "MeshUtils.h"
 
-std::vector<GLfloat> mjt::utils::MeshUtils::createSquare(int x, int y, int width, int height)
+mjt::utils::MeshData mjt::utils::MeshUtils::createSquare(GLfloat x, GLfloat y, GLfloat width, GLfloat height, 
+	GLfloat texX, GLfloat texY, GLfloat texWidth, GLfloat texHeight)
 {
-	return std::vector<GLfloat>();
+	GLfloat v[] = {
+		x, y, 0,
+		x + width, y, 0,
+		x, y + height, 0,
+		x + width, y + height, 0
+	};
+	std::vector<GLfloat> vertices(v, v + 12);
+
+	GLfloat u[] = {
+		texX, texY,
+		texX + texWidth, texY,
+		texX, texY + texHeight,
+		texX + texWidth, texY + texHeight
+	};
+	std::vector<GLfloat> uvs(u, u + 8);
+
+	GLint i[] = {
+		0, 1, 2,
+		1, 3, 2
+	};
+	std::vector<GLint> indices(i, i + 6);
+
+	mjt::utils::MeshData meshData = {
+		vertices,
+		uvs,
+		indices
+	};
+
+	return meshData;
 }
--- CharacterAnimation/MJT/Utils/MeshUtils.h
+++ CharacterAnimation/MJT/Utils/MeshUtils.h
@@ -7,11 +7,21 @@ namespace mjt
 {
 	namespace utils
 	{
+		struct MeshData
+		{
+			std::vector<GLfloat> vertices;
+			std::vector<GLfloat> uvs;
+			std::vector<GLint> indices;
+		};
+
 		class MeshUtils
 		{
 		public:
 			// Create a square model composed of 4 vertices
-			static std::vector<GLfloat> createSquare(int x, int y, int width, int height);
+			static MeshData createSquare(
+				GLfloat x, GLfloat y, GLfloat width, GLfloat height,
+				GLfloat texX = 0, GLfloat texY = 0, GLfloat texWidth = 1, GLfloat texHeight = 1
+			);
 		};
 	}
 }
